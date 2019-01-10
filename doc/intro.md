ab5e8fa4d201b6798c64f26a37cd9c92c0b94ee2
kazesberger <klaus.azesberger@bearingpoint.com>
2019-01-10 16:13:41 +0100

    Create prj using 'lein new'

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to tex-kata
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/tex_kata/core_test.clj
@@ -0,0 +1,7 @@
+(ns tex-kata.core-test
+  (:require [clojure.test :refer :all]
+            [tex-kata.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
