b7f4c079eda248ea5d402302d6f3e20592dfcbc8
kazesberger <klaus.azesberger@gmail.com>
2019-01-10 21:08:29 +0100

    Solve part1

--- src/tex_kata/core.clj
+++ src/tex_kata/core.clj
@@ -1,6 +1,13 @@
-(ns tex-kata.core)
+(ns tex-kata.core
+  (:require [clojure.string :as str]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def puzzle-input (str/split (slurp "resources/y18d02") #"\s+"))
+
+(defn idcount-by-letter-dups [id-list duplications]
+  (->> id-list
+       (map frequencies)
+       (map vals)
+       (filter (partial some #{duplications}))
+       (count)))
+
+(apply * (map (partial idcount-by-letter-dups puzzle-input) [2 3]))
